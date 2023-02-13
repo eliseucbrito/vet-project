0800e3c7540eec801b5efc224bdb81ebc5a37fbe
eliseubrito7z <eliseubrito776@gmail.com>
2023-02-12 23:03:53 -0300

    fix: user is setting correctly

---
+++ src/components/Cards/NextPatients.tsx
@@ -0,0 +1,38 @@
+import { HStack, Stack, Text, VStack } from '@chakra-ui/react'
+
+export function NextPatients() {
+  return (
+    <Stack bg="white" w="100%" h="100%" p="1.25rem" borderRadius={12}>
+      <Text>Próximos pacientes</Text>
+      <VStack w="100%" align="start" px="1.25rem">
+        <ul>
+          <li>Eliseu</li>
+        </ul>
+        <ul>
+          <li>Eliseu</li>
+        </ul>
+        <ul>
+          <li>Eliseu</li>
+        </ul>
+        <ul>
+          <li>Eliseu</li>
+        </ul>
+        <ul>
+          <li>Eliseu</li>
+        </ul>
+        <ul>
+          <li>Eliseu</li>
+        </ul>
+        <ul>
+          <li>Eliseu</li>
+        </ul>
+        <ul>
+          <li>Eliseu</li>
+        </ul>
+        <ul>
+          <li>Eliseu</li>
+        </ul>
+      </VStack>
+    </Stack>
+  )
+}
